09a3b002cc84ea2a1098489331754df22dfbdd51
Danielit707 <ceradaniel517@gmail.com>
2024-04-13 22:12:06 -0500

    first commit

---
+++ src/pizzeria/Pizzeria.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pizzeria;
+
+import java.util.List;
+
+/**
+ *
+ * @author decera
+ */
+public class Pizzeria {
+    private String nombre;
+    private List<Cliente> clientes;
+    private List<Item> items;
+    private List<Pedido> pedidos;
+    
+    public boolean addCliente(Cliente cliente){
+        
+    }
+    
+    public boolean addItem (Item item){
+        
+    }
+    
+    public boolean addPedido (Pedido pedido){
+        
+    }
+    
+    public Cliente getCliente (int index){
+        
+    }
+    
+    public item getItem (int index){
+        
+    }
+    
+    public int calcProdMasVendidoCliente(int numCliente){
+        
+    }
+    
+    
+    
+}
